2fbc581de11fc6dfea98302160195cb21d2f14a2
90mani <manikandan.c@sankitsolutions.com>
2023-08-31 12:19:49 +0530

    app creation

---
+++ studentproject/studentnamelist/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def student(request):
+    return HttpResponse("i am saravanan")
+
+# Create your views here.
---
+++ studentproject/studentrollno/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StudentrollnoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'studentrollno'
