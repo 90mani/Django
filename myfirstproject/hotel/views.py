7730733aa6112401bad83b556482e9f64a70cf7c
90mani <manikandan.c@sankitsolutions.com>
2023-09-05 13:49:19 +0530

    database

--- myfirstproject/hotel/views.py
+++ myfirstproject/hotel/views.py
@@ -1,46 +1,67 @@
+
 from pipes import Template
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+import mysql.connector
+
+
+
+
+mydb = mysql.connector.connect(
+    host="127.0.0.1",
+    user="root",
+    password="2810",
+    database="studend",
+)
+mycursor = mydb.cursor()
 
 
 def homepage(request):
-    return HttpResponse("It me django")
-
+     return HttpResponse("It me django")
 
 def firstpage(request):
-    template = loader.get_template("first.html")
-    return HttpResponse(template.render())
-
+  template=loader.get_template("first.html")
+  return HttpResponse(template.render())
 
 def colour(request):
-    template = loader.get_template("test1.html")
-    return HttpResponse(template.render())
+  template=loader.get_template("test1.html")
+  return HttpResponse(template.render())
 
 
 def lastpage(request):
-    template = loader.get_template("name.html")
-    name = "kamalam"
-    context = {"firstname": name, "lastname": "doss"}
-    return HttpResponse(template.render(context, request))
-
+  template=loader.get_template("name.html")
+  name = "kamalam"
+  context = {"firstname": name, "lastname": "doss"}
+  return HttpResponse(template.render(context, request))
 
 def mark(request):
-    template = loader.get_template("ifelse.html")
-    context = {"firstname": "kamalam", "mark": 50}
-    return HttpResponse(template.render(context, request))
-
+  template=loader.get_template("ifelse.html")
+  context = {"firstname": "kamalam", "mark":50}
+  return HttpResponse(template.render(context, request))
 
 def tablecreate(request):
-    template = loader.get_template("table.html")
-    studentlist = [
-        {"name": "kamalam", "id": 1221, "mark": 65, "grade": 3},
-        {"name": "jeeva", "id": 1223, "mark": 80, "grade": 2},
-        {"name": "mani", "id": 1224, "mark": 70, "grade": 1},
-    ]
-    print(studentlist)
-    context = {"studentdata": studentlist}
-    return HttpResponse(template.render(context, request))
+   template = loader.get_template("table.html")
+   studentlist=[
+        {"name": "kamalam","id":1221, "mark":65, "grade":3},
+        {"name":"jeeva", "id":1223, "mark":80, "grade":2},
+        {"name":"mani", "id":1224, "mark":70, "grade":1},
+               ]
+   
+   context = {"studentdata": studentlist}
+   mycursor.execute("SELECT * from marksheet")
+   myresult = mycursor.fetchall()
+   return HttpResponse(template.render(context, request))
+
+
+def logicfile(request):
+  template=loader.get_template("logic.html")
+  return HttpResponse(template.render())
 
 
 # Create your views here.
+
+
+
+
+
